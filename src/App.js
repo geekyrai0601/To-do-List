66b7bf7eaba5b6c7aafc4a83a089584551e4be10
geekyrai0601 <raiswapnil0601@gmail.com>
2024-09-26 08:38:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+// import './style.css';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((item, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="cards">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Add a new todo"
+      />
+      <button class= "b" onClick={addTodo}>Add Todo</button>
+      <TodoList todos={todos} onDelete={deleteTodo} />
     </div>
   );
-}
+};
 
 export default App;
